f4f994345b7c753561780e24bb7a045ffc458818
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-05-21 13:17:56 +0200

    add Bagels Game logic for 1 game

--- 01_Bagels/bagels.py
+++ 01_Bagels/bagels.py
@@ -1,21 +1,56 @@
-def help():
-    print("\t Pico  = 1 digit is correct (right position)")
-    print("\t Fermi = 1 digit is correct (wrong position)")
-    print("\t Bagels= no digit is correct")
+# TODO: generate a random 3 digit variable
+win_val = 123; # fixed value for implementing and testing
+win_first_digit = 1;
+win_second_digit = 2;
+win_third_digit = 3;
 
 def game():
     # Prompt user
     print("Insert a 3 digit number: \n")
     # Read input and convert Input from user 
-    input_val = input()
-    # TODO: Check and output (pico, fermi, bagels)
-    # TODO: Check if input is "-h" and print helper
-    print(input_val)
+    input_val = int(input())
+    #Check and output (pico, fermi, bagels)
+        # get the 3 values
+    first_digit = input_val//100;
+    second_digit = (input_val//10)%10;
+    third_digit = input_val%10;
+        #flag for bagels
+    bagels_fg = True
+
+    #check for pico (position, value correct)
+    if win_first_digit == first_digit:
+        print("pico ", end="")
+        bagels_fg = False
+    # check for fermi (value correct, position wrong)
+    elif (win_first_digit == second_digit or
+          win_first_digit == third_digit):
+        print("fermi ", end="")
+        bagels_fg = False
+    if win_second_digit == second_digit:
+        print("pico ", end="")
+        bagels_fg = False
+    elif (win_second_digit == first_digit or 
+          win_second_digit == third_digit):
+        print("fermi ", end="")
+        bagels_fg = False
+
+    if win_third_digit == third_digit:
+        print("pico ", end="")
+        bagels_fg = False
+    elif (win_third_digit == first_digit or 
+          win_third_digit == second_digit):
+        print("fermi ", end="")
+        bagels_fg = False 
+
+    #check for bagels (all wrong)
+    if bagels_fg == True:
+        print("bagels")
 
 # Start Program: 
 print("Welcome to the bagels game, try to guess a 3 digit number \n")
-print("Here are some helpful informations: (you can always print them by inserting -h)")
-help()
+print("\t Pico  = 1 digit is correct (right position)")
+print("\t Fermi = 1 digit is correct (wrong position)")
+print("\t Bagels= no digit is correct")
 
 # play 10 rounds
 game()
