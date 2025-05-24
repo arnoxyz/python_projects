b1666da14d38b24c8a461eb09cbc59ba9637e202
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-05-24 11:11:53 +0200

    BirthdayPara divide and plan program

---
+++ 02_BirthdayParadox/birthdaypara.py
@@ -0,0 +1,19 @@
+# Simulate Birthdayparadox
+
+#TODO: make my life easier with the first protoype by hardcoding: 
+# number of birthdays just 2
+# number of sims just 1
+import random
+
+def main():
+    #TODO: Input: Number of Birthdays
+    #TODO: Input: Number of Simulations
+
+
+        #TODO: genBirthdays(int NumberOfBirthdays)
+        #TODO: checkBirthday(Birthdays[]) 
+            #-> return 
+            #   true=win (there are two birthdays on the same day)
+            #   false=lose (no birthdays on the same day)
+    #TODO: calcPropability
+        # prop=Wins/NumberOfSims
