40bcbc188c545bb09ffa5cf0fa1a84ef24be45d8
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-05-25 16:06:02 +0200

    BirthdayPara check_duplicate_birthdays() done

--- 02_BirthdayParadox/birthdaypara.py
+++ 02_BirthdayParadox/birthdaypara.py
@@ -14,21 +14,37 @@ def gen_birthdays():
     # Format: day.month (e.g., 5.3 for 5th March)
     return f"{random_date.day}.{random_date.month}"
 
+
+# check if there is are two birthdays on the same date
+# return true if there is one, false if there is none
+def check_dublicate_birthdays(bdays): 
+    for i in range(len(bdays)):
+        for j in range(i + 1, len(bdays)):
+            if bdays[i] == bdays[j]:
+                return True
+    return False
+
 def main():
     #make my life easier with the first protoype by hardcoding: 
     bdays_number = 10 #TODO: Get Input from user: Number of Birthdays
     sim_number   = 1 #TODO: Get Input from user: Number of Simulations
+    wins = 0;
 
-    #TODO: add loop number of sim here
-    print("bla")
-    #genBirthdays(int NumberOfBirthdays)
-    birthdays = [gen_birthdays() for _ in range(bdays_number)]
-    print(birthdays)
+    for _ in range(sim_number):
+        #genBirthdays(int NumberOfBirthdays)
+        birthdays = [gen_birthdays() for _ in range(bdays_number)]
+        print(birthdays)
 
-        #TODO: checkBirthday(Birthdays[]) 
+        #checkBirthday(Birthdays[]) 
+        if (check_dublicate_birthdays(birthdays) == True): 
+            wins+=1;
+
+        print(wins)
+        #brute foce check the birthdays O(n^2)
             #-> return 
             #   true=win (there are two birthdays on the same day)
             #   false=lose (no birthdays on the same day)
+
     #TODO: calcPropability
         # prop=Wins/NumberOfSims
 
