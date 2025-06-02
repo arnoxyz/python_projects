b1fd1dbfdf5f121e3f25e25bfdfcb8f360ca4269
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-06-02 22:33:26 +0200

    BitMapMsg add other version to draw a bitmap

--- 03_BitmapMessage/bitmapmsg.py
+++ 03_BitmapMessage/bitmapmsg.py
@@ -1,7 +1,7 @@
 # Implementation of Bitmap Message
 # Displays a user input text in a given bitmap by replacing symbols in the bitmap with the given input symbols.
 
-# Example the Austria Flag
+# Example: the Austria Flag
 bitmap = """
 
 
@@ -22,16 +22,29 @@ bitmap = """
 """
 
 
-#replace_with = "Austria"
-replace_with = input("insert a word: ")
+def draw_bitmap(bitmap):
+    replace_with = input("insert a word: ")
+    j = 0;
+    for i in bitmap:
+        if i=='*':
+            print(replace_with[j], end="")
+            j=j+1
+            if j == len(replace_with):
+                j=0
+        else:
+            print(i, end="")
 
-j = 0;
-for i in bitmap:
-    if i=='*':
-        print(replace_with[j], end="")
-        j=j+1
-        if j == len(replace_with):
-            j=0
-    else:
-        print(i, end="")
-    
+# Other version using modulo operator
+def draw_bitmap2(bitmap):
+    replace_with = input("insert a word: ")
+    j = 0;
+    for i in bitmap:
+        if i=='*':
+            print(replace_with[j % len(replace_with)], end="")
+            j=j+1
+        else:
+            print(i, end="")
+
+
+draw_bitmap(bitmap)
+draw_bitmap2(bitmap)
