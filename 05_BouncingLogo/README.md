64381acbdb2120dee2430a95037ac68c38f669ac
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-06-12 14:05:47 +0200

    BouncingLogo install Module Bext with demo()

--- 05_BouncingLogo/README.md
+++ 05_BouncingLogo/README.md
@@ -1,2 +1,14 @@
 #  Bouncing Logo
 Simulating the bouncing logo of the dvd screen
+
+
+## Bext (Module)
+Using the module best (https://github.com/asweigart/bext)
+### Installation using a Virtual-Env
+    - python3 -m venv venv 
+    - source venv/bin/acivate
+    - pip install bext
+#### Activate it
+    - source venv/bin/activate
+#### Deactivate it
+    - deactivate
--- 05_BouncingLogo/bouncinglogo.py
+++ 05_BouncingLogo/bouncinglogo.py
@@ -1,12 +1,24 @@
 # Implementation of BouncingLogo 
 
+# using module bext by Al Sweigart (https://github.com/asweigart/bext)
+import bext, random
 
+# demo of bext
+def demo():
+    width, height = bext.size()
 
+    try:
+        while True:
+            bext.fg('random')
+            bext.bg('random')
+            x = random.randint(0, width - 1)
+            y = random.randint(0, height - 1)
 
-def main():
-    print("Hello World!")
+            if x == width -1 and y == height - 1:
+                continue 
+            bext.goto(x, y)
+            print('*', end='')
+    except KeyboardInterrupt:
+        pass
 
-main()
-
-
-
+demo()
