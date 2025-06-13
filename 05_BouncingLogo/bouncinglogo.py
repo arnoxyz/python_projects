5a469234715ea29eb5efdbc5c0ac14e92976145d
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-06-14 00:21:48 +0200

    BouncingLogo my bext init

--- 05_BouncingLogo/bouncinglogo.py
+++ 05_BouncingLogo/bouncinglogo.py
@@ -3,22 +3,10 @@
 # using module bext by Al Sweigart (https://github.com/asweigart/bext)
 import bext, random
 
-# demo of bext
-def demo():
-    width, height = bext.size()
+def main():
+    width, height = bext.size() #return current terminal size 
+    bext.fg('black') #text-color
+    bext.bg('white') 
+    bext.clear()
 
-    try:
-        while True:
-            bext.fg('random')
-            bext.bg('random')
-            x = random.randint(0, width - 1)
-            y = random.randint(0, height - 1)
-
-            if x == width -1 and y == height - 1:
-                continue 
-            bext.goto(x, y)
-            print('*', end='')
-    except KeyboardInterrupt:
-        pass
-
-demo()
+main()
