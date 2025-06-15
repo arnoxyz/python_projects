fc00d9b56abe7dcf827e1d276337d1087a0d7d24
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-06-15 20:08:19 +0200

    BouncinLogo add move x

--- 05_BouncingLogo/bouncinglogo.py
+++ 05_BouncingLogo/bouncinglogo.py
@@ -1,39 +1,43 @@
 # Implementation of BouncingLogo 
 # using module bext by Al Sweigart (https://github.com/asweigart/bext)
-#import bext
+import bext, time, sys
 
-# move x-pos
-def move_x_pos(pos):
-    pos["x"] += 1
-    #bext.goto(pos["x"],pos["y"])
-
-# move y-pos
-def move_y_pos(pos):
-    pos["y"] += 1
-    #bext.goto(pos["x"],pos["y"])
-
-def display(text):
-    #bext.clear()
-    print(text)
 
 def main():
-    text = "TEXT"
-    pos = {"x" : 0, "y" : 0}
-    print(pos)
-    move_x_pos(pos)
-    print(pos)
-    move_y_pos(pos)
-    print(pos)
-
-
-def bext_stuff():
-    width, height = bext.size() #return current terminal size 
-    bext.fg('black') #text-color
-    bext.bg('white') 
     bext.clear()
-    move_x_pos(pos)
-    move_y_pos(pos)
-    print(pos, end="")
-    display(text)
+    bext.fg('black') 
+    bext.bg('white') 
+    text = "A" 
+    width, height = bext.size() #return current terminal size 
+
+    # Coordinate: (x,y) 
+    x = 0;
+    y = 0;
+
+    # Window: 
+    # (0,0) --------------------------------- (Width-1, 0)
+    # (0,1) --------------------------------- (Width-1, 1)
+    # (0,2) --------------------------------- (Width-1, 2)
+    # ...
+    # ...
+    # (0,Height-3) -------------------------- (Width-1, Height-3)
+    # (0,Height-2) -------------------------- (Width-1, Height-2)
+    # (0,Height-1) -------------------------- (Width-1, Height-1)
+    
+    # Corners:
+    left_up = (0,0)
+    right_up = (width-1,0)
+    right_down = (width-1,height-1)
+    left_down = (0,height-1)
+
+    while True: 
+        bext.clear();
+        bext.goto(x,y)
+
+        if x < width-1: 
+            x = x+1
+        print(text, end="")
+        sys.stdout.flush()
+        time.sleep(0.5)
 
 main()
