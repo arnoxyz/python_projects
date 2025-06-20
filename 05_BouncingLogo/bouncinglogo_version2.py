02f01055b9a865b2c4d4d21c48a04b9c9e8cc484
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-06-20 15:01:39 +0200

    BouncingLogo version2 done moving logos

--- 05_BouncingLogo/bouncinglogo_version2.py
+++ 05_BouncingLogo/bouncinglogo_version2.py
@@ -7,8 +7,8 @@ def gen_logos(number, width, height, colors, directions):
     for i in range(number):
         logos.append({
             "color"     : random.choice(colors), 
-            "x"         : random.randint(1, width - 4),
-            "y"         : random.randint(1, height - 4),
+            "x"         : random.randint(0, width-1),
+            "y"         : random.randint(0, height-1),
             "direction" : random.choice(directions)
         })
     return logos
@@ -20,28 +20,49 @@ def display_logo(logo):
     bext.goto(0,0)
     sys.stdout.flush()
 
-def move_logos(logos):
+def move_logos(logos, width, height):
     for logo in logos:
+        # Check Edges before moving 
+        if logo["x"] <= 0 and logo["direction"] == "ul": 
+            logo["direction"] = "ur"
+        elif logo["x"] <= 0 and logo["direction"] == "dl": 
+            logo["direction"] = "dr"
+        elif logo["x"] >= width-3 and logo["direction"] == "ur": 
+            logo["direction"] = "ul"
+        elif logo["x"] >= width-3 and logo["direction"] == "dr": 
+            logo["direction"] = "dl"
+
+        elif logo["y"] <= 1 and logo["direction"] == "ul": 
+            logo["direction"] = "dl"
+        elif logo["y"] <= 1 and logo["direction"] == "ur": 
+            logo["direction"] = "dr"
+        elif logo["y"] >= height-2 and logo["direction"] == "dl": 
+            logo["direction"] = "ul"
+        elif logo["y"] >= height-2 and logo["direction"] == "dr": 
+            logo["direction"] = "ur"
+
+        # Move logos
         if logo["direction"] == "ur":
-            logo["x"] += 2
+            logo["x"] += 1
             logo["y"] -= 1
         elif logo["direction"] == "ul":
-            logo["x"] -= 2
+            logo["x"] -= 1
             logo["y"] -= 1
         elif logo["direction"] == "dr":
-            logo["x"] += 2
+            logo["x"] += 1
             logo["y"] += 1
         elif logo["direction"] == "dl":
-            logo["x"] -= 2
+            logo["x"] -= 1
             logo["y"] += 1
         display_logo(logo)
         
 
 
 def main(): 
-    pause = 0.2
+    bext.hide_cursor()
+    pause = 0.01 
     colors  = ['red', 'green', 'blue']
-    number_of_logos = 5
+    number_of_logos = 10 
     directions = ('ur', 'ul', 'dr', 'dl') #u=up, d=down, r=right, l=left
     width, height = bext.size()
 
@@ -51,8 +72,9 @@ def main():
     logos = gen_logos(number_of_logos, width, height, colors, directions);
 
     # move logos
-    move_logos(logos)
-    move_logos(logos)
-    move_logos(logos)
+    while True:
+        move_logos(logos, width, height)
+        time.sleep(pause)
+
 
 main()
