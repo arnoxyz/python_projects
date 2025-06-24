e4e0514672121c2dfead28342e33727a9ae4e976
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-06-24 12:50:15 +0200

    CesarCipher done make it work with fixed text input

--- 06_CeasarCipher/cesarcipher.py
+++ 06_CeasarCipher/cesarcipher.py
@@ -1,3 +1,24 @@
 # Implementaion of CaesarCipher
-print("Hello World!");
 
+text = "Hello World!"
+print("Input Test: " + text)
+
+
+def shift_text(text, shift): 
+    result = ""
+    for char in text: 
+        if char.isalpha():
+            if char.isupper(): 
+                base = ord("A")
+            else:
+                base = ord("a")
+            current_position = ord(char) - base
+            new_position = (current_position + shift) % 26
+            #need to add back the base to get the right ascii number
+            result += chr(new_position + base) 
+        else:
+            result += char
+    return result
+
+
+print("Output Text: " + shift_text(text, 16))
