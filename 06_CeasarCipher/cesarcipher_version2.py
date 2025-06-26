121b06337d5a6ade0be078dbe791bd8302dfe1cc
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-06-26 13:47:22 +0200

    CesarCipher version2 refact and add program details

--- 06_CeasarCipher/cesarcipher_version2.py
+++ 06_CeasarCipher/cesarcipher_version2.py
@@ -3,10 +3,19 @@
 try:
     import pyperclip
 except ImportError:
-    print("pyperclip not installed, so output will not be copied to clipboard...");
+    print("pyperclip not installed, so output will not be copied to clipboard...")
 
-try:
-    pyperclip.copy("Hello World!")
-    print("Output copied to clipboard");
-except:
-    pass
+def copy_text_to_clipboard(text):
+    try:
+        pyperclip.copy("Hello World!")
+        print("Output copied to clipboard")
+    except:
+        pass
+
+text = "User Text!"
+# User Input: Encrypt/Decrypt
+# User Input: Key (shift number) number % 26
+# User Input: Text (the text that should get en-/decrypted
+# Text processing and saving/outputing result
+
+copy_text_to_clipboard(text)
