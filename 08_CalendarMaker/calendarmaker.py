a626252f7bbf4c469036ad48197cba20c0bd24a0
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-07-03 17:26:10 +0200

    CalendarMaker add module and config

--- 08_CalendarMaker/calendarmaker.py
+++ 08_CalendarMaker/calendarmaker.py
@@ -1,6 +1,22 @@
 # Implementaion of a calender maker
+import calendar
+
+
+def user_input(config):
+    config["month"] = input("Input a valid month -> ");
+    config["year"] = input("Input a valid year -> ");
 
 def main():
     print("Hello World!")
+    # input year and moth -> output using datetime module from python
+    # fixed values for now
+    config = {
+                "month" : 6,
+                "year" : 2025
+    }
+
+    # config = user_input(config)
+
+    print(calendar.month(config["year"], config["month"]))
 
 main()
