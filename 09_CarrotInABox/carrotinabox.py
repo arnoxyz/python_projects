6f265a2bed504f5758f4be051c3955d7399511c7
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-07-11 16:04:28 +0200

    CarrotInABox add draw_boxes

--- 09_CarrotInABox/carrotinabox.py
+++ 09_CarrotInABox/carrotinabox.py
@@ -1,6 +1,22 @@
 # Implementaion of a two player game called: Carrot In a Box
 
+def draw_boxes():
+    box = ""
+    top    =     "+--------+  +--------+"
+    empty_line = "|        |  |        |"
+    middle =     "|  BOX1  |  |  BOX2  |"
+    bottom =     "+--------+  +--------+"
+
+    box += top + "\n"
+    box += empty_line + "\n"
+    box += middle + "\n"
+    box += empty_line + "\n"
+    box += bottom + "\n"
+
+    print(box)
+
 def main():
-    print("Hello World!")
+    print("Welcome to Carrot in a Box!")
+    draw_boxes()
 
 main()
