61b701e34903cf79bc1c0673c116cc0e9e108f29
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-07-13 15:23:40 +0200

    CarrotINABox add give either player1 or player2 the object

--- 09_CarrotInABox/carrotinabox.py
+++ 09_CarrotInABox/carrotinabox.py
@@ -1,41 +1,37 @@
 # Implementaion of a two player game called: Apple In a Box
+import random
+
+def random_bit():
+    return random.randint(0, 1)
 
 def draw_boxes():
     box = ""
+    names  =     "-PLAYER 1-  -PLAYER 2-"
     top    =     "+--------+  +--------+"
     empty_line = "|        |  |        |"
     middle =     "|  BOX1  |  |  BOX2  |"
     bottom =     "+--------+  +--------+"
 
+    box += names+ "\n"
     box += top + "\n"
     box += empty_line + "\n"
     box += middle + "\n"
     box += empty_line + "\n"
     box += bottom + "\n"
 
-    print(box)
-
-def draw_open_box1():
-    box = ""
-    top    =     "+        +  +--------+"
-    empty_line = "|        |  |        |"
-    middle =     "|  BOX1  |  |  BOX2  |"
-    bottom =     "+--------+  +--------+"
-
-    box += top + "\n"
-    box += empty_line + "\n"
-    box += middle + "\n"
-    box += empty_line + "\n"
-    box += bottom + "\n"
     print(box)
 
 def draw_open_boxes1():
     box = ""
-    top    =     "+ (---)  +  +        +"
+    names  =     "-PLAYER 1-  -PLAYER 2-"
+    empty =      "                      "
+    top    =     "  (---)               "
     empty_line = "|        |  |        |"
     middle =     "|  BOX1  |  |  BOX2  |"
     bottom =     "+--------+  +--------+"
 
+    box += names + "\n"
+    box += empty + "\n"
     box += top + "\n"
     box += empty_line + "\n"
     box += middle + "\n"
@@ -45,11 +41,15 @@ def draw_open_boxes1():
 
 def draw_open_boxes2():
     box = ""
-    top    =     "+        +  +  (---) +"
+    names  =     "-PLAYER 1-  -PLAYER 2-"
+    empty =      "                      "
+    top    =     "               (---)  "
     empty_line = "|        |  |        |"
     middle =     "|  BOX1  |  |  BOX2  |"
     bottom =     "+--------+  +--------+"
 
+    box += names + "\n"
+    box += empty + "\n"
     box += top + "\n"
     box += empty_line + "\n"
     box += middle + "\n"
@@ -62,7 +62,13 @@ def main():
     print("Welcome to Apple in a Box!")
     draw_boxes()
 
-    draw_open_boxes1()
-    draw_open_boxes2()
+    config = {
+                "player1 got apple" : True if random_bit() == 1 else False
+    }
+
+    if (config["player1 got apple"]):
+        draw_open_boxes1()
+    else:
+        draw_open_boxes2()
 
 main()
