2b0c980d8ce47ac11eb669206bec6bc1e1e00bc1
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-07-17 18:34:02 +0200

    CarrotInABox version2 add show boxes

--- 09_CarrotInABox/carrotinabox_version2.py
+++ 09_CarrotInABox/carrotinabox_version2.py
@@ -5,14 +5,30 @@ def get_names(players):
     input("Press Enter to begin...")
     players["player1_name"] = input("Name Player1: ")
     players["player2_name"] = input("Name Player2: ")
-    return players;
-
-
     return players
+
+def draw_boxes(draw, players):
+    # only draw winning box (that with the item inside)
+    box = ""
+    box     += "╔══════╗ \n"
+    box     += "║--WIN-║ \n"
+    box     += "╚══════╝ \n"
+
+    if draw=="init":
+        print(box)
+
+    if draw=="player1 won":
+        print(box)
+        print(players["player1_name"])
+
+    if draw=="player2 won":
+        print(box)
+        print(players["player2_name"])
 
 
 
 def main():
+
     players = {
             "player1_name" : "Player 1",
             "player2_name" : "Player 2"
@@ -21,6 +37,7 @@ def main():
     players = get_names(players);
     print(players)
 
+    draw_boxes("init", players);
 
 
 main()
