358cc132badfb8464dbbabd6436a82b2ad1ae31e
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-07-23 23:00:11 +0200

    ChoHan add player_inputs

--- 10_ChoHan/chohan.py
+++ 10_ChoHan/chohan.py
@@ -64,7 +64,35 @@ def draw_dice(face1, face2):
     for line1, line2 in zip(die1, die2):
         print(f"{line1}   {line2}")
 
-def main():
+def set_player_decision(player_config):
+    while True:
+        user_input = input("Is the sum of the dice (E)ven or (O)dd? > ").lower()
+        if user_input.startswith("e"):
+            player_config["decision"] = "e"
+            break
+        elif user_input.startswith("o"):
+            player_config["decision"] = "o"
+            break
+        else:
+            print("Invlaid input insert: e or o");
+
+
+def set_player_bet(player_config):
+    print("Your credit is " + str(player_config["credit"]))
+
+    while True:
+        user_input = input("How much do you want to bet? > ")
+        if user_input.isdigit():
+            if player_config["credit"] >= int(user_input):
+                player_config["credit"] = player_config["credit"] - int(user_input)
+                player_config["bet"] = int(user_input)
+                break
+            else:
+                print("Invalid Input to big bet for current credit!")
+        else:
+            print("Invlaid input insert a valid number")
+
+def game():
     # show empty dice
     draw_dice("empty", "empty")
 
@@ -72,7 +100,23 @@ def main():
     dice1 = throw_dice()
     dice2 = throw_dice()
 
+    # betting
+
     # show solution
     draw_dice(dice1, dice2)
+
+    # win or lose
+
+def main():
+    player_config = {
+        "credit" : 100,
+        "bet" : 0,
+        "decision" : ""
+    }
+
+    set_player_bet(player_config)
+    set_player_decision(player_config)
+
+    print(player_config)
 
 main()
