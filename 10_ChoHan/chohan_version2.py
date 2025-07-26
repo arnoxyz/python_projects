2db19e2a0edc48227de3eac59b46c0a7ca31f96c
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-07-26 14:35:05 +0200

    choHan Version2 add roll_dice()

--- 10_ChoHan/chohan_version2.py
+++ 10_ChoHan/chohan_version2.py
@@ -18,14 +18,21 @@ def place_bet(money):
             pot = int(pot)
             return pot
 
+def roll_dice():
+    dice1 = random.randint(1, 6)
+    dice2 = random.randint(1, 6)
+    return dice1+dice2;
+
+
 def main():
     money = 5000
     pot = 0
+    dice_sum = 0
 
     pot = place_bet(money)
 
-    print("Hello World!")
-
+    print("The dealer takes the dice and puts them into a cup. Then he swirls the cup and you hear the rattle of dice.  The dealer slams the cup on the floor, still covering the dice and asks for your bet. Insert CHO (for even) or HAN (for odd) dice sum?")
+    dice_sum = roll_dice()
 
 
 main()
