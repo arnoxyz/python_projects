f17192ba200a6f4e534e1a96cc0946fc52102712
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-07-26 14:29:20 +0200

    ChoHan Version2 add palce_bet()

--- 10_ChoHan/chohan_version2.py
+++ 10_ChoHan/chohan_version2.py
@@ -1,7 +1,31 @@
 # Another Implementaion of cho/han
-import random
+import random, sys
+
+JAPANESE_NUMBERS = {1: 'ICHI', 2: 'NI', 3: 'SAN', 4: 'SHI', 5: 'GO', 6: 'ROKU'}
+
+def place_bet(money):
+    print("You have ", money, " money. How much do you bet? (or stop)")
+    while True:
+        pot = input("> ")
+        if pot.upper() == 'STOP':
+            print("Thanks for playing!")
+            sys.exit()
+        elif not pot.isdecimal():
+            print("Please enter a number.")
+        elif int(pot) > money:
+            print("You do not have enough to make that bet.")
+        else:
+            pot = int(pot)
+            return pot
 
 def main():
+    money = 5000
+    pot = 0
+
+    pot = place_bet(money)
+
     print("Hello World!")
+
+
 
 main()
