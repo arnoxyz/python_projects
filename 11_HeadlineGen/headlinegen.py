08a87d24e3475c9e05cf6e723360088a639a8dd6
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-08-11 11:00:10 +0200

    Headline add get user input

--- 11_HeadlineGen/headlinegen.py
+++ 11_HeadlineGen/headlinegen.py
@@ -1,7 +1,15 @@
 # Implementaion of clickbait headline genrator
 import random
 
+def get_input():
+    while True:
+        user_input = input("How many headlines do you want to generate? > ")
+        if user_input.isdigit():
+            return int(user_input)
+
+
 def main():
     print("Hello World")
+    print(get_input())
 
 main()
