81845f6ecde93e2645aaaf7117d9f67ecc84f744
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-08-11 11:06:03 +0200

    Headline add constants, set number of headlines to 1 for now, start with gen_headline()

--- 11_HeadlineGen/headlinegen.py
+++ 11_HeadlineGen/headlinegen.py
@@ -1,15 +1,32 @@
 # Implementaion of clickbait headline genrator
 import random
 
+#constants
+OBJECT_PRONOUNS = ['Her', 'Him', 'Them']
+POSSESIVE_PRONOUNS = ['Her', 'His', 'Their']
+PERSONAL_PRONOUNS = ['She', 'He', 'They']
+STATES = ['California', 'Texas', 'Florida', 'New York', 'Pennsylvania', 'Illinois', 'Ohio', 'Georgia', 'North Carolina', 'Michigan']
+NOUNS = ['Athlete', 'Clown', 'Shovel', 'Paleo Diet', 'Doctor', 'Parent', 'Cat', 'Dog', 'Chicken', 'Robot', 'Video Game', 'Avocado', 'Plastic Straw','Serial Killer', 'Telephone Psychic']
+PLACES = ['House', 'Attic', 'Bank Deposit Box', 'School', 'Basement', 'Workplace', 'Donut Shop', 'Apocalypse Bunker']
+WHEN = ['Soon', 'This Year', 'Later Today', 'RIGHT NOW', 'Next Week']
+
 def get_input():
     while True:
         user_input = input("How many headlines do you want to generate? > ")
         if user_input.isdigit():
             return int(user_input)
 
+def gen_headline():
+    headline = random.randint(1,8)
+    print(headline)
+
 
 def main():
     print("Hello World")
-    print(get_input())
+    #number_of_headlines = get_input()
+    number_of_headlines = 1
+
+    for i in range(number_of_headlines):
+        gen_headline()
 
 main()
