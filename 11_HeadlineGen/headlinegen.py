c0151404d41bf2dd33d071a50cb8b66949b26464
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-08-14 18:04:39 +0200

    HeadlineGen add get_headline, to create random headlines

--- 11_HeadlineGen/headlinegen.py
+++ 11_HeadlineGen/headlinegen.py
@@ -18,19 +18,55 @@ def get_input():
 
 def gen_headline(number_of_headlines):
     for i in range(number_of_headlines):
-        headline_number = random.randint(1,8)
+        headline_number = random.randint(1,7)
         headline = get_headline(headline_number)
         print(headline)
 
 def get_headline(number):
-    return "test"
+    headline = ""
+
+    if number == 1:
+        noun = random.choice(NOUNS)
+        headline = "Are Millennials Killing the {} Industry?".format(noun)
+    elif number == 2:
+        noun = random.choice(NOUNS)
+        pluralNoun = random.choice(NOUNS) + 's'
+        when = random.choice(WHEN)
+        headline = "Without This {}, {} Could Kill You {}".format(noun, pluralNoun, when)
+    elif number == 3:
+        pronoun = random.choice(OBJECT_PRONOUNS)
+        state = random.choice(STATES)
+        noun1 = random.choice(NOUNS)
+        noun2 = random.choice(NOUNS)
+        headline = "Big Companies Hate {}! See How This {} {} Invented a Cheaper {}".format(pronoun, state, noun1, noun2)
+    elif number == 4:
+        state = random.choice(STATES)
+        noun = random.choice(NOUNS)
+        pronoun = random.choice(POSSESIVE_PRONOUNS)
+        place = random.choice(PLACES)
+        headline = "You Won\'t Believe What This {} {} Found in {} {}".format(state, noun, pronoun, place)
+    elif number == 5:
+        pluralNoun1 = random.choice(NOUNS) + 's'
+        pluralNoun2 = random.choice(NOUNS) + 's'
+        headline = "What {} Don\'t Want You To Know About {}".format(pluralNoun1, pluralNoun2)
+    elif number == 6:
+        number = random.randint(7, 15)
+        noun = random.choice(NOUNS)
+        state = random.choice(STATES)
+        headline = "{} Gift Ideas to Give Your {} From {}".format(number, noun, state)
+    elif number == 7:
+        number1 = random.randint(3, 19)
+        pluralNoun = random.choice(NOUNS) + 's'
+        number2 = random.randint(1, number1)
+        headline = "{} Reasons Why {} Are More Interesting Than You Think (Number {} Will Surprise You!)".format(number1, pluralNoun, number2)
+
+    return headline
 
 
 def main():
     print("Hello World")
     #number_of_headlines = get_input()
     number_of_headlines = 1
-
     gen_headline(number_of_headlines)
 
 main()
