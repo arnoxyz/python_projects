f0f20dda05e72f38fa5656881a9b2dcdcd678630
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-08-15 22:38:55 +0200

    Collatz add gen one collatz iteration

--- 12_CollatzSequence/collatz.py
+++ 12_CollatzSequence/collatz.py
@@ -1,4 +1,5 @@
 # Generation of the collatz sequence
+import math
 
 def get_user_input():
     while True:
@@ -6,8 +7,16 @@ def get_user_input():
         if number.isdigit():
             return int(number)
 
+def generate_collatz_sequence(number):
+    if (number % 2) == 0:
+        return int(number/2)
+    else:
+        return 3*number+1
+
 def main():
-    number = get_user_input()
-    print(number)
+    #number = get_user_input()
+    number = 20
+    print(generate_collatz_sequence(number))
+
 
 main()
