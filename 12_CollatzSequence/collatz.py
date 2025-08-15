804178d1f0ba6db60769fb9d0e14bdb2832ee153
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-08-15 17:58:31 +0200

    Collatz add get_user_input

--- 12_CollatzSequence/collatz.py
+++ 12_CollatzSequence/collatz.py
@@ -1,5 +1,13 @@
 # Generation of the collatz sequence
+
+def get_user_input():
+    while True:
+        number = input("Insert start integer > ")
+        if number.isdigit():
+            return int(number)
+
 def main():
-    print("Hello World!")
+    number = get_user_input()
+    print(number)
 
 main()
