1d8079dc3385574cc7016e903552d883d2ea1883
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-08-16 20:40:16 +0200

    Collatz add check first 10k integers if they convert to the 4 2 1 sequence

--- 12_CollatzSequence/collatz.py
+++ 12_CollatzSequence/collatz.py
@@ -7,19 +7,34 @@ def get_user_input():
         if number.isdigit():
             return int(number)
 
-def generate_collatz_sequence(number, idx):
+def generate_collatz_sequence(number, seen=None):
     print(number, end=" ")
 
-    if idx != 1:
-        if (number % 2) == 0:
-            generate_collatz_sequence(int(number/2), idx-1)
-        else:
-            generate_collatz_sequence(3*number+1, idx-1)
+    if seen is None:
+        seen = set()
+
+    seen.add(number)
+
+    if {4, 2, 1}.issubset(seen):
+        return
+
+    if (number % 2) == 0:
+        generate_collatz_sequence(number//2, seen)
+    else:
+        generate_collatz_sequence(3*number+1, seen)
+
+def gen_first_10k():
+    for idx in range(1, 10000):
+        print()
+        generate_collatz_sequence(idx)
+        print()
 
 def main():
     #number = get_user_input()
-    number = 20
-    generate_collatz_sequence(number, 100)
+    #number = 20
+    #generate_collatz_sequence(number, 100)
+
+    gen_first_10k()
 
 
 main()
