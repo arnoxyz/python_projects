65b254071b6678c9625585fb72cfceb7b50cf5fa
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-08-18 17:52:29 +0200

    chapter2 game_of_life add fill_grid, print_grid

--- chapter2/12_game_of_life/game_of_life.py
+++ chapter2/12_game_of_life/game_of_life.py
@@ -1,4 +1,27 @@
+# Implementation of the game of life
+
+def fill_grid(width, height):
+    grid = {}
+    ALIVE = "1"
+    DEAD  = "0"
+    for x in range(width):
+        for y in range(height):
+            grid[(x, y)] = DEAD
+    return grid;
+
+def print_grid(grid, width, height):
+    for y in range(height):
+        for x in range(width):
+            print(grid[(x,y)], end="")
+        print()
+
 def main():
+    width = 10
+    height = 10
+
     print("Hello World!")
+    grid = fill_grid(width, height)
+    print_grid(grid,width, height)
+
 
 main()
