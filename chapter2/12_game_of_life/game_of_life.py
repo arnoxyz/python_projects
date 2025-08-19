fb1b8456cd5331e17c50812e9c93098580209804
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-08-19 18:42:24 +0200

    chapter2 game_of_life add check neighbor cells and count alive ones

--- chapter2/12_game_of_life/game_of_life.py
+++ chapter2/12_game_of_life/game_of_life.py
@@ -1,12 +1,14 @@
 # Implementation of the game of life
 
+ALIVE = "1"
+DEAD  = "0"
+
 def fill_grid(width, height):
     grid = {}
-    ALIVE = "1"
-    DEAD  = "0"
     for x in range(width):
         for y in range(height):
-            grid[(x, y)] = DEAD
+            #grid[(x, y)] = DEAD
+            grid[(x, y)] = ALIVE
     return grid;
 
 def print_grid(grid, width, height):
@@ -15,13 +17,37 @@ def print_grid(grid, width, height):
             print(grid[(x,y)], end="")
         print()
 
+def check_grid(grid, width, height):
+    actual_value = 0;
+
+    for x in range(width):
+        for y in range(width):
+            current_cell = grid[(x,y)]
+            alive_cells = 0
+
+            #get cells around the actual_cell
+            left_cell   = grid[(x - 1) % width, y]
+            right_cell  = grid[(x + 1) % width, y]
+            top_cell    = grid[x, (y - 1) % height]
+            bottom_cell = grid[x, (y + 1) % height]
+
+            if left_cell == ALIVE:
+                alive_cells = alive_cells + 1;
+            if right_cell == ALIVE:
+                alive_cells = alive_cells + 1;
+            if top_cell == ALIVE:
+                alive_cells = alive_cells + 1;
+            if bottom_cell == ALIVE:
+                alive_cells = alive_cells + 1;
+            print(alive_cells)
+        print()
+
 def main():
     width = 10
     height = 10
 
-    print("Hello World!")
     grid = fill_grid(width, height)
-    print_grid(grid,width, height)
-
+    #print_grid(grid, width, height)
+    check_grid(grid, width, height)
 
 main()
