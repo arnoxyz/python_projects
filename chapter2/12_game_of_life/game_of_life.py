3262ec5caed24edb49305402b2269fcd0e9f64a0
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-08-19 20:12:34 +0200

    chapter2 game_of_life add apply conways rules on the current grid

--- chapter2/12_game_of_life/game_of_life.py
+++ chapter2/12_game_of_life/game_of_life.py
@@ -1,4 +1,5 @@
 # Implementation of the game of life
+import copy
 
 ALIVE = "1"
 DEAD  = "0"
@@ -7,8 +8,9 @@ def fill_grid(width, height):
     grid = {}
     for x in range(width):
         for y in range(height):
-            #grid[(x, y)] = DEAD
-            grid[(x, y)] = ALIVE
+            grid[(x, y)] = DEAD
+            #grid[(x, y)] = ALIVE
+
     return grid;
 
 def print_grid(grid, width, height):
@@ -19,18 +21,20 @@ def print_grid(grid, width, height):
 
 def check_grid(grid, width, height):
     actual_value = 0;
+    next_grid = copy.deepcopy(grid)
 
     for x in range(width):
-        for y in range(width):
+        for y in range(height):
             current_cell = grid[(x,y)]
             alive_cells = 0
 
-            #get cells around the actual_cell
+            #get neighbor cells (all cells around the actual_cell)
             left_cell   = grid[(x - 1) % width, y]
             right_cell  = grid[(x + 1) % width, y]
             top_cell    = grid[x, (y - 1) % height]
             bottom_cell = grid[x, (y + 1) % height]
 
+            #count all neighbors that are alive
             if left_cell == ALIVE:
                 alive_cells = alive_cells + 1;
             if right_cell == ALIVE:
@@ -39,8 +43,18 @@ def check_grid(grid, width, height):
                 alive_cells = alive_cells + 1;
             if bottom_cell == ALIVE:
                 alive_cells = alive_cells + 1;
-            print(alive_cells)
-        print()
+
+            #apply rule
+            if current_cell == ALIVE and (alive_cells == 2 or alive_cells == 3):
+                #current Cell stays Alive
+                next_grid[(x,y)] = ALIVE
+            elif current_cell == DEAD and (alive_cells == 3):
+                #current Cell becomes Alive
+                next_grid[(x,y)] = ALIVE
+            else:
+                next_grid[(x,y)] = DEAD
+
+    print_grid(next_grid, width, height)
 
 def main():
     width = 10
