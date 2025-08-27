6b330dc155fcf006cfa13369c2394e9d0bfdae35
arnoxyz <83603053+arnoxyz@users.noreply.github.com>
2025-08-27 15:34:48 +0200

    chapter2 add cave pattern

--- chapter2/14_cave/cave.py
+++ chapter2/14_cave/cave.py
@@ -1,23 +1,34 @@
-import random, sys
+import random, sys, time
 
-
-def createCave():
+def createCave(start):
     #one line is always 100 symbols
-
-    #one line is [left-part] [middle-part] [right-part]
-    middle = 60
-
+    middle = start
     left = (100-middle)//2
     right = left
 
+    #one line is [left-part] [middle-part] [right-part]
     line = "*" * left + "-" * middle  +"*"* right;
-    print(line)
+    print(line, flush=True)
 
 
 
 def main():
     print("Hello World!")
-    createCave()
+    start = random.randint(0, 100)
+    while True:
+        choice = random.randint(0,2)
+        if choice == 0:
+            start = start+5
+        elif choice == 1:
+            start = start-5
+
+        if start <= 0:
+            start = 10
+        if start >= 100:
+            start = 90
+
+        createCave(start)
+        time.sleep(0.05)
 
 
 main()
